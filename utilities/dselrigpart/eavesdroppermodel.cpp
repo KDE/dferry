0b1eefb8e38fa3632d5d7858dc366d3e2accf5d8
Andreas Hartmetz <ahartmetz@gmail.com>
2013-03-03 16:57:23 +0100

    Include more information in EavesdropperModel.

--- utilities/dselrigpart/eavesdroppermodel.cpp
+++ utilities/dselrigpart/eavesdroppermodel.cpp
@@ -7,9 +7,51 @@
 #include "message.h"
 #include "transceiver.h"
 
+#include <QDateTime>
 #include <QSocketNotifier>
 
 #include <vector>
+
+struct MessageRecord
+{
+    MessageRecord(Message *msg)
+       : message(msg),
+         timestamp(QDateTime::currentDateTime())
+    {}
+    QString type() const;
+    // the serial of the "conversation", i.e. request-response pair
+    uint32 conversationSerial() const;
+    uint latency() const; // ## do this here or in some proxy model?
+
+    Message *message;
+    QDateTime timestamp;
+};
+
+QString MessageRecord::type() const
+{
+    switch (message->type()) {
+    case Message::MethodCallMessage:
+        return QObject::tr("Call");
+    case Message::MethodReturnMessage:
+        return QObject::tr("Return");
+    case Message::ErrorMessage:
+        return QObject::tr("Error");
+    case Message::SignalMessage:
+        return QObject::tr("Signal");
+    case Message::InvalidMessage:
+        return QString::fromLatin1("???");
+    }
+    Q_ASSERT(false);
+    return QString();
+}
+
+uint32 MessageRecord::conversationSerial() const
+{
+    if (message->type() == Message::MethodReturnMessage || message->type() == Message::ErrorMessage) {
+        return message->replySerial();
+    }
+    return message->serial();
+}
 
 static void fillEavesdropMessage(Message *spyEnable, const char *messageType)
 {
@@ -37,7 +79,7 @@ public:
     void messageReceived(Message *message);
 
     EavesdropperModel *q;
-    std::vector<Message *> messages;
+    std::vector<MessageRecord> messages;
 
     EpollEventDispatcher *dispatcher;
     Transceiver *transceiver;
@@ -51,7 +93,7 @@ EavesdropperModel::Private::Private(EavesdropperModel *model)
 EavesdropperModel::Private::~Private()
 {
     for (int i = 0; i < messages.size(); i++) {
-        delete messages[i];
+        delete messages[i].message;
     }
     delete dispatcher;
     delete transceiver;
@@ -103,7 +145,7 @@ EavesdropperModel::~EavesdropperModel()
 void EavesdropperModel::addMessage(Message *message)
 {
     beginInsertRows(QModelIndex(), d->messages.size(), d->messages.size());
-    d->messages.push_back(message);
+    d->messages.push_back(MessageRecord(message));
     endInsertRows();
 }
 
@@ -111,22 +153,24 @@ QVariant EavesdropperModel::data(const QModelIndex &index, int role) const
 {
     if (role == Qt::DisplayRole) {
         Q_ASSERT(index.row() < d->messages.size());
-        Message *m = d->messages[index.row()];
+        const MessageRecord &mr = d->messages[index.row()];
         switch (index.column()) {
-        case 0: {
-            const std::string method = m->method();
+        case 0:
+            return mr.type();
+        case 1: {
+            const std::string method = mr.message->method();
             return QString::fromUtf8(method.c_str(), method.length());
         }
-        case 1: {
-            const std::string iface = m->interface();
+        case 2: {
+            const std::string iface = mr.message->interface();
             return QString::fromUtf8(iface.c_str(), iface.length());
         }
-        case 2: {
-            const std::string sender = m->sender();
+        case 3: {
+            const std::string sender = mr.message->sender();
             return QString::fromUtf8(sender.c_str(), sender.length());
         }
-        case 3: {
-            const std::string destination = m->destination();
+        case 4: {
+            const std::string destination = mr.message->destination();
             return QString::fromUtf8(destination.c_str(), destination.length());
         }
         default:
@@ -141,12 +185,14 @@ QVariant EavesdropperModel::headerData(int section, Qt::Orientation orientation,
     if (role == Qt::DisplayRole && orientation == Qt::Horizontal) {
         switch (section) {
         case 0:
-            return tr("Method");
+            return tr("Type");
         case 1:
-            return tr("Interface");
+            return tr("Method");
         case 2:
-            return tr("Sender");
+            return tr("Interface");
         case 3:
+            return tr("Sender");
+        case 4:
             return tr("Destination");
         default:
             break;
@@ -182,5 +228,5 @@ int EavesdropperModel::rowCount(const QModelIndex &parent) const
 
 int EavesdropperModel::columnCount(const QModelIndex &parent) const
 {
-    return 4;
+    return 5;
 }
