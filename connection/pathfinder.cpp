ce2d0215882ceac98e0fb45570d2f25e5fd8e07a
Andreas Hartmetz <ahartmetz@gmail.com>
2013-03-06 13:57:26 +0100

    Add copyright headers!
    LGPLv2+ because that's the default for me, and MPL for static linking.
    This comment is not part of the license agreement and is not meant to
    alter the licensing in any way.

--- connection/pathfinder.cpp
+++ connection/pathfinder.cpp
@@ -1,3 +1,26 @@
+/*
+   Copyright (C) 2013 Andreas Hartmetz <ahartmetz@gmail.com>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LGPL.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+
+   Alternatively, this file is available under the Mozilla Public License
+   Version 1.1.  You may obtain a copy of the License at
+   http://www.mozilla.org/MPL/
+*/
+
 #include "pathfinder.h"
 
 #include "stringtools.h"
